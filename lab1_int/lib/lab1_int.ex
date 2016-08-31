4ea0364b854d26f51fb02d542034ae3feb2fda63
Michael Pope <mpope.cr@gmail.com>
2016-08-31 09:41:05 -0400

    Finish lab1_int word counter

--- lab1_int/lib/lab1_int.ex
+++ lab1_int/lib/lab1_int.ex
@@ -1,2 +1,10 @@
 defmodule Lab1Int do
+  def count(str) do
+    String.split(str)
+    |> Enum.reduce(%{}, &count_word/2)
+  end
+
+  defp count_word(word, acc) do
+    Map.update(acc, word, 1, fn i -> i + 1 end)
+  end
 end
--- lab1_int/test/lab1_int_test.exs
+++ lab1_int/test/lab1_int_test.exs
@@ -1,8 +1,19 @@
 defmodule Lab1IntTest do
   use ExUnit.Case
-  doctest Lab1Int
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def count(str) do
+    Lab1Int.count(str)
+  end
+
+  test "word counter" do
+    assert count("") == %{}
+
+    assert count("          ") == %{}
+
+    assert count("foo") == %{"foo" => 1}
+
+    assert count("foo bar") == %{"foo" => 1, "bar" => 1}
+
+    assert count("foo bar foo foo bar") == %{"foo" => 3, "bar" => 2}
   end
 end
